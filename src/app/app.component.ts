9aa6fb5b17009c9f0d75a89d40f4bbe1ba7a059e
Damian Sobanski <dam.sob690@wp.pl>
2025-05-09 16:08:17 +0200

    Initial UI improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,4 +25,64 @@ export class AppComponent {
   toggleDescription(taskId: number) {
     this.descVisible[taskId] = !this.descVisible[taskId];
   }
+
+  toggleTaskStatus(task: any) {
+    switch (task.status) {
+      case ('Planned'): {
+        task.status = 'Pending';
+        break;
+      }
+      case ('Pending'): {
+        task.status = 'Completed';
+        break;
+      }
+      case ('Completed'): {
+        task.status = 'Planned';
+        break;
+      }
+      default: {
+        task.status = 'Planned';
+        break;
+      }
+    }
+  }
+
+  getDeadlineDaysLeft(dateString: string): number {
+    const today = new Date();
+    const todayMidnight = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    const deadline = new Date(dateString);
+    const deadlineMidnight = new Date(deadline.getFullYear(), deadline.getMonth(), deadline.getDate());
+    const milisecondsInDay = 1000*60*60*24;
+
+    const diffInMs = deadlineMidnight.getTime() - todayMidnight.getTime();
+    return Math.floor(diffInMs / milisecondsInDay);
+  }
+
+  getDeadlineLabel(dateString: string): string {
+    const diffDays = this.getDeadlineDaysLeft(dateString);
+
+    if (diffDays < 0) {
+      return 'Termin minął';
+    } else if (diffDays === 0) {
+      return 'Termin dzisiaj';
+    } else if (diffDays === 1) {
+      return diffDays + ' dzień';
+    } else if (diffDays <= 30) {
+      return diffDays + ' dni';
+    } else {
+      const months = Math.floor(diffDays / 30)
+      return months + ' mies.';
+    }
+  }
+
+  getDeadlineClass(dateString: string): string {
+    const diffDays = this.getDeadlineDaysLeft(dateString);
+    if (diffDays < 0) {
+      return 'text-danger';
+    } else if (diffDays <= 3) {
+      return 'text-warning';
+    } else {
+      return 'text-muted';
+    }
+  }
 }
